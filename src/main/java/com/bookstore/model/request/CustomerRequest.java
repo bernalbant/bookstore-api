0599e9a32ac2c4b2cd722d960a8baa9c57832207
Berkay Ernalbant <berkayernalbant@gmail.com>
2021-03-28 20:38:35 +0300

    add request and response models

---
+++ src/main/java/com/bookstore/model/request/CustomerRequest.java
@@ -0,0 +1,28 @@
+package com.bookstore.model.request;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CustomerRequest {
+
+  @NotNull(message = "Name field is required")
+  @Size(min = 1, message = "Name field is required")
+  private String name;
+
+  @NotNull(message = "Last Name field is required")
+  @Size(min = 1, message = "Last Name field is required")
+  private String surname;
+
+  @NotNull(message = "Phone Number field is required")
+  @Pattern(regexp = "^\\d{10}$", message = "Phone Number format is invalid")
+  private String phone;
+
+  @NotNull(message = "Email field is required")
+  @Size(min = 1, message = "Email field is required")
+  private String email;
+
+  private String address;
+}
