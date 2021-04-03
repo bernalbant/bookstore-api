830855ba51bf3d12d039c429d287cf32e4d49fab
Berkay Ernalbant <berkayernalbant@gmail.com>
2021-04-03 20:05:02 +0300

    refactoring

--- src/main/java/com/bookstore/handler/BookHandler.java
+++ src/main/java/com/bookstore/handler/BookHandler.java
@@ -1,31 +1,41 @@
 package com.bookstore.handler;
 
+import com.bookstore.converter.BookConverter;
+import com.bookstore.converter.BookListConverter;
 import com.bookstore.model.entity.Book;
 import com.bookstore.model.request.BookRequest;
 import com.bookstore.model.response.BookListResponse;
 import com.bookstore.model.response.BookResponse;
 import com.bookstore.service.BookService;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookHandler {
 
+  private BookConverter bookConverter;
+  private BookListConverter bookListConverter;
   private BookService bookService;
 
-  public BookHandler(BookService bookService) {
+  public BookHandler(BookConverter bookConverter, BookService bookService, BookListConverter bookListConverter) {
+    this.bookConverter = bookConverter;
+    this.bookListConverter = bookListConverter;
     this.bookService = bookService;
   }
 
   public BookListResponse findAllBooks() {
-    return null;
+    List<Book> bookList = bookService.findAll();
+    return bookListConverter.convert(bookList);
   }
 
   public BookResponse findBookById(int id) {
-    return null;
+    Book book = bookService.findById(id);
+    return bookConverter.convert(book);
   }
 
   public BookResponse createBook(BookRequest bookRequest) {
-    return null;
+    Book savedBook = bookService.save(bookConverter.convert(bookRequest));
+    return bookConverter.convert(savedBook);
   }
 
   public void deleteBook(int id) {
--- src/main/java/com/bookstore/handler/CustomerHandler.java
+++ src/main/java/com/bookstore/handler/CustomerHandler.java
@@ -1,31 +1,44 @@
 package com.bookstore.handler;
 
+import com.bookstore.converter.CustomerConverter;
+import com.bookstore.converter.CustomerListConverter;
 import com.bookstore.model.entity.Customer;
 import com.bookstore.model.request.CustomerRequest;
 import com.bookstore.model.response.CustomerListResponse;
 import com.bookstore.model.response.CustomerResponse;
 import com.bookstore.service.CustomerService;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerHandler {
 
   private CustomerService customerService;
+  public CustomerConverter customerConverter;
+  public CustomerListConverter customerListConverter;
 
-  public CustomerHandler(CustomerService customerService) {
+  public CustomerHandler(CustomerService customerService,
+                         CustomerConverter customerConverter,
+                         CustomerListConverter customerListConverter) {
     this.customerService = customerService;
+    this.customerConverter = customerConverter;
+    this.customerListConverter = customerListConverter;
   }
 
   public CustomerListResponse findAllCustomers() {
-    return null;
+    List<Customer> customers = customerService.findAll();
+    return customerListConverter.convert(customers);
   }
 
   public CustomerResponse findCustomerById(int id) {
-    return null;
+    Customer customer = customerService.findById(id);
+
+    return customerConverter.convert(customer);
   }
 
   public CustomerResponse createCustomer(CustomerRequest customerRequest) {
-    return null;
+    Customer savedCustomer = customerService.save(customerConverter.convert(customerRequest));
+    return customerConverter.convert(savedCustomer);
   }
 
   public void deleteCustomer(int id) {
