8461175e0f3ed0001e62a43aba7d211a541768be
Berkay Ernalbant <berkayernalbant@gmail.com>
2021-04-23 09:06:09 +0300

    exception constants added

---
+++ src/main/java/com/bookstore/constant/ExceptionConstant.java
@@ -0,0 +1,9 @@
+package com.bookstore.constant;
+
+public class ExceptionConstant {
+
+  public static final String CUSTOMER_NOT_FOUND_MESSAGE = "Customer not found with the given id ";
+  public static final String BOOK_NOT_FOUND_MESSAGE = "Book not found with the given id ";
+  public static final String ORDER_NOT_FOUND_MESSAGE = "Order not found with the given id ";
+  public static final String INSUFFICIENT_STOCK_MESSAGE = "Insufficient stock";
+}
